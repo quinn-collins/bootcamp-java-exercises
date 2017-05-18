bd7df75e94545a28faed1fcf6ad0d82b7f8f2362
Quinn Collins <collinsqui@gmail.com>
2017-05-18 11:57:52 -0400

    finished all w2d4 class exercises

--- m1-w2d4-introduction-to-classes/src/main/java/com/techelevator/calculator/Calculator.java
+++ m1-w2d4-introduction-to-classes/src/main/java/com/techelevator/calculator/Calculator.java
@@ -1,9 +1,34 @@
 package com.techelevator.calculator;
 
 public class Calculator {
-
-	//
-	// Write code here
-	//
+	private int currentValue;
+	
+	public int getCurrentValue(){
+		return currentValue;
+	}
+	
+	public int add(int addend){
+		currentValue += addend;
+		return currentValue;
+	}
+	
+	public int multiply(int multiplier){
+		currentValue *= multiplier;
+		return currentValue;
+	}
+	
+	public int subtract(int subtrahend){
+		currentValue -= subtrahend;
+		return currentValue;
+	}
+	
+	public int power(int exponent){
+		currentValue = (int)Math.pow(currentValue, exponent);
+		return currentValue;
+	}
+	
+	public void reset(){
+		currentValue = 0;
+	}
 	
 }
--- m1-w2d4-introduction-to-classes/src/main/java/com/techelevator/dog/Dog.java
+++ m1-w2d4-introduction-to-classes/src/main/java/com/techelevator/dog/Dog.java
@@ -1,9 +1,28 @@
 package com.techelevator.dog;
 
 public class Dog {
+	private boolean sleeping;
+	
+	public boolean isSleeping(){
+		return sleeping;
+	}
+	
+	public String makeSound(){
+		if(sleeping){
+			return("Zzzzz...");
+		}
+		else{
+			return("Woof!");
+		}
+	}
+	
+	public void sleep(){
+		sleeping = true;
+	}
+	
+	public void wakeUp(){
+		sleeping = false;
+	}
 
-	//
-	// Write code here
-	//
 	
 }
