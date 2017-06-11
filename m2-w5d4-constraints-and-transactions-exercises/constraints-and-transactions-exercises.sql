37233d4253185fe6d269c7f72f4c5584491aa20d
Quinn Collins <collinsqui@gmail.com>
2017-06-11 14:49:22 -0400

    completed week 5 day 4 exercises

--- m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises.sql
+++ m2-w5d4-constraints-and-transactions-exercises/constraints-and-transactions-exercises.sql
@@ -2,24 +2,62 @@
 -- The following changes are applied to the "pagila" database.**
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
+SELECT *
+FROM actor;
 
+INSERT INTO actor (first_name, last_name) VALUES ('HAMPTON', 'AVENUE');
+INSERT INTO actor (first_name, last_name) VALUES ('LISA', 'BYWAY');
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in 
 -- ancient Greece", to the film table. The movie was released in 2008 in English. 
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special 
 -- features, the film speaks for itself, and doesn't need any gimmicks.	
+SELECT *
+FROM film
+WHERE length = 198;
 
+INSERT INTO film (title, description, release_year, language_id, length) 
+VALUES ('EUCLIDEAN PI', 'The epic story of Euclid as a pizza delivery boy in ancient Greece', 2008, 1, 198);
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly 
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
+SELECT *
+FROM actor
+WHERE first_name = 'HAMPTON' OR first_name = 'LISA';
 
+SELECT *
+FROM film
+WHERE title = 'Euclidean PI';
+
+INSERT INTO film_actor (actor_id, film_id) 
+VALUES ('201', '1001');
+INSERT INTO film_actor (actor_id, film_id) 
+VALUES ('202', '1001');
 -- 4. Add Mathmagical to the category table.
-
+INSERT INTO category (name)
+VALUES ('Mathmagical');
+SELECT *
+FROM category;
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI", 
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
+INSERT INTO film_category (film_id, category_id)
+VALUES (1003, 17);
+INSERT INTO film_category (film_id, category_id)
+VALUES (274, 17);
+INSERT INTO film_category (film_id, category_id)
+VALUES (494, 17);
+INSERT INTO film_category (film_id, category_id)
+VALUES (714, 17);
+INSERT INTO film_category (film_id, category_id)
+VALUES (996, 17);
 
+SELECT *
+FROM film
+WHERE title = 'EGG IGBY' OR title = 'KARATE MOON' OR title = 'RANDOM GO' OR title = 'YOUNG LANGUAGE' OR title = 'EUCLIDEAN PI';
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films 
 -- accordingly.
 -- (5 rows affected)
-
+UPDATE film
+SET rating='G'
+WHERE title = 'EGG IGBY' OR title = 'KARATE MOON' OR title = 'RANDOM GO' OR title = 'YOUNG LANGUAGE' OR title = 'EUCLIDEAN PI';
 -- 7. Add a copy of "Euclidean PI" to all the stores.
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film, 
